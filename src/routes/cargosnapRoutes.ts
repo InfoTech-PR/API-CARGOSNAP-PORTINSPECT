1635e642e6a47a04a54b7b307dcbd8a03be73279
mauboru <josue21henrique@gmail.com>
2025-02-21 11:26:10 -0300

    rotas thunder teste configuradas e estrutura para funções ok

--- src/routes/cargosnapRoutes.ts
+++ src/routes/cargosnapRoutes.ts
@@ -3,7 +3,15 @@ import * as cargosnapController from "../controllers/cargosnapController";
 
 const router = Router();
 
-router.get("/getArquivos", cargosnapController.getArquivos);
-router.post("/createRecurso", cargosnapController.createRecurso);
+router.get("/getFiles", cargosnapController.getFiles);
+router.get("/getFilesById/", cargosnapController.getFilesById);
+router.post("/setFiles", cargosnapController.setFiles);
+router.patch("/closeFilesById", cargosnapController.closeFilesById);
+router.delete("/deleteFilesById", cargosnapController.deleteFilesById);
+router.post("/uploadsFiles", cargosnapController.uploadsFiles);
+router.post("/fieldsFiles", cargosnapController.fieldsFiles);
+router.post("/reportsFiles", cargosnapController.reportsFiles);
+router.get("/shareFiles", cargosnapController.shareFiles);
+router.get("/formsFilesById", cargosnapController.formsFilesById);
 
 export default router;
