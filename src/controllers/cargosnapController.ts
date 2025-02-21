d42fdd7b1f6644152c1b2926b794e66d5697acfb
mauboru <josue21henrique@gmail.com>
2025-02-21 11:02:46 -0300

    estruturando todas as rotas

--- src/controllers/cargosnapController.ts
+++ src/controllers/cargosnapController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { cargosnapRequest } from "../services/cargosnapService";
 
-export const getArquivos = async (req: Request, res: Response) => {
+//GET
+export const getFiles = async (req: Request, res: Response) => {
   try {
     const data = await cargosnapRequest("/files", "GET");
     res.json(data);
@@ -10,12 +11,21 @@ export const getArquivos = async (req: Request, res: Response) => {
   }
 };
 
-export const createRecurso = async (req: Request, res: Response) => {
-  try {
-    const { nome, descricao } = req.body;
-    const data = await cargosnapRequest("/create-resource", "POST", { nome, descricao });
-    res.json(data);
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
-};
+//GET
+export const getFilesById = async (req: Request, res: Response) => {}
+//POST
+export const setFiles = async (req: Request, res: Response) => {}
+//PATCH
+export const closeFilesById = async (req: Request, res: Response) => {}
+//DELETE
+export const deleteFilesById = async (req: Request, res: Response) => {}
+//POST
+export const uploadsFiles = async (req: Request, res: Response) => {}
+//POST
+export const fieldsFiles = async (req: Request, res: Response) => {}
+//POST
+export const reportsFiles = async (req: Request, res: Response) => {}
+//GET
+export const shareFiles = async (req: Request, res: Response) => {}
+//GET
+export const formsFilesById = async (req: Request, res: Response) => {}
