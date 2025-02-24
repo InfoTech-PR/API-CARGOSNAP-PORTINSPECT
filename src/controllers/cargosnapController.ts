a40502b0a3ee409c4ff9eb52f8cbd8fccf5af92b
mauboru <josue21henrique@gmail.com>
2025-02-24 08:55:32 -0300

    rota para api getById ok

--- src/controllers/cargosnapController.ts
+++ src/controllers/cargosnapController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { cargosnapRequest } from "../services/cargosnapService";
 
-//GET
 export const getFiles = async (req: Request, res: Response) => {
   try {
     const data = await cargosnapRequest("/files", "GET");
@@ -11,8 +10,17 @@ export const getFiles = async (req: Request, res: Response) => {
   }
 };
 
-//GET
-export const getFilesById = async (req: Request, res: Response) => {}
+export const getFilesById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ error: "O ID do arquivo é obrigatório."});
+    
+    const data = await cargosnapRequest(`/files/${id}`, "GET");
+    res.json(data);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+}
 //POST
 export const setFiles = async (req: Request, res: Response) => {}
 //PATCH
--- src/routes/cargosnapRoutes.ts
+++ src/routes/cargosnapRoutes.ts
@@ -4,14 +4,14 @@ import * as cargosnapController from "../controllers/cargosnapController";
 const router = Router();
 
 router.get("/getFiles", cargosnapController.getFiles);
-router.get("/getFilesById/", cargosnapController.getFilesById);
+router.get("/getFilesById/:id", cargosnapController.getFilesById);
 router.post("/setFiles", cargosnapController.setFiles);
-router.patch("/closeFilesById", cargosnapController.closeFilesById);
-router.delete("/deleteFilesById", cargosnapController.deleteFilesById);
+router.patch("/closeFilesById/:id/close", cargosnapController.closeFilesById);
+router.delete("/deleteFilesById/:id", cargosnapController.deleteFilesById);
 router.post("/uploadsFiles", cargosnapController.uploadsFiles);
 router.post("/fieldsFiles", cargosnapController.fieldsFiles);
 router.post("/reportsFiles", cargosnapController.reportsFiles);
 router.get("/shareFiles", cargosnapController.shareFiles);
-router.get("/formsFilesById", cargosnapController.formsFilesById);
+router.get("/formsFilesById/:id/forms", cargosnapController.formsFilesById);
 
 export default router;
