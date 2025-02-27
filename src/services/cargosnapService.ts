3f915eefb5f00f2497a31e2edec6a71b85c6846b
Josue Henrique <josue21henrique@gmail.com>
2025-02-27 11:37:56 -0300

    closeFilesById ok

--- src/services/cargosnapService.ts
+++ src/services/cargosnapService.ts
@@ -1,26 +1,29 @@
 import axios from "axios";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const API_URL = process.env.CARGOSNAP_URL;
 const API_TOKEN = process.env.CARGOSNAP_API_KEY;
 
-export const cargosnapRequest = async (endpoint: string, method: "GET" | "POST" | "PATCH" | "DELETE", params?: any) => {
+export const cargosnapRequest = async (
+  endpoint: string,
+  method: "GET" | "POST" | "PATCH" | "DELETE",
+  params?: any
+) => {
   try {
-    const url = `${API_URL}${endpoint}?token=${API_TOKEN}&format=json`;
+    let url = `${API_URL}${endpoint}?token=${API_TOKEN}`;
+
+    if (params) {
+      const queryString = new URLSearchParams(params).toString();
+      url += `&${queryString}`;
+    }
 
     const config: any = {
       url,
       method,
       headers: { "Content-Type": "application/json" },
     };
-
-    if (method === "POST" || method === "PATCH") {
-      config.data = params;
-    } else if (params) {
-      config.params = { ...config.params, ...params };
-    }
-    console.log("Request Config:", config); // Debug
 
     const response = await axios(config);
     return response.data;
