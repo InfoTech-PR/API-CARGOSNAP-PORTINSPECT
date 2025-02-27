496467e0e664d2c5addd27d042b37362a78831d0
Josue Henrique <josue21henrique@gmail.com>
2025-02-27 11:07:20 -0300

    corrigindo rota get

--- src/services/cargosnapService.ts
+++ src/services/cargosnapService.ts
@@ -3,23 +3,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const API_URL = process.env.CARGOSNAP_URL;
+const API_TOKEN = process.env.CARGOSNAP_API_KEY;
 
 export const cargosnapRequest = async (endpoint: string, method: "GET" | "POST" | "PATCH" | "DELETE", params?: any) => {
   try {
-    const queryParams = new URLSearchParams(params);
-    queryParams.append('token', process.env.CARGOSNAP_API_KEY || '');
-    queryParams.append('format', 'json');
+    const url = `${API_URL}${endpoint}?token=${API_TOKEN}&format=json`;
 
-    const url = `${API_URL}${endpoint}?${queryParams.toString()}`;
-    const response = await axios({
+    const config: any = {
       url,
       method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: method === "POST" ? params : null,
-    });
+      headers: { "Content-Type": "application/json" },
+    };
 
+    if (method === "POST" || method === "PATCH") {
+      config.data = params;
+    } else if (params) {
+      config.params = { ...config.params, ...params };
+    }
+    console.log("Request Config:", config); // Debug
+
+    const response = await axios(config);
     return response.data;
   } catch (error: any) {
     console.error("Erro na requisição:", error.response?.data || error.message);
