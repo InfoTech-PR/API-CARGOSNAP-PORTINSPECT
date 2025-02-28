66bd309eb1af6014a7b1bfa8c53fe8569a62c039
Josue Henrique <josue21henrique@gmail.com>
2025-02-28 09:41:17 -0300

    modificada estrutura de todas as rotas

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import cargosnapRoutes from "./cargosnapRoutes";
+import * as cargosnapController from "../controllers/cargosnapController";
 
 const router = Router();
 
-router.use("/cargosnap", cargosnapRoutes);
+router.post("/files", cargosnapController.setFiles);
+router.get("/files", cargosnapController.getFiles);
+router.get("/files/:id", cargosnapController.getFilesById);
+router.patch("/files/:id/close", cargosnapController.closeFilesById);
+router.delete("/files/:id/delete", cargosnapController.deleteFilesById);
+router.post("/uploads", cargosnapController.uploadsFiles);
+router.post("/fields", cargosnapController.fieldsFiles);
+router.post("/reports", cargosnapController.reportsFiles);
+router.get("/share", cargosnapController.shareFiles);
+router.get("/forms/:id", cargosnapController.formsFilesById);
 
 export default router;
