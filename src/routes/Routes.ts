7a7e72ebbbd0471ebe35fcef87848a27485e4d58
Josue Henrique <josue21henrique@gmail.com>
2025-02-28 10:24:25 -0300

    rota uploads funcionando

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
 import * as cargosnapController from "../controllers/cargosnapController";
 
 const router = Router();
+const upload = multer({ dest: "uploads/" });
 
 router.post("/files", cargosnapController.setFiles);
 router.get("/files", cargosnapController.getFiles);
 router.get("/files/:id", cargosnapController.getFilesById);
 router.patch("/files/:id/close", cargosnapController.closeFilesById);
 router.delete("/files/:id/delete", cargosnapController.deleteFilesById);
-router.post("/uploads", cargosnapController.uploadsFiles);
+router.post("/uploads", upload.array("uploads", 10), cargosnapController.uploadsFiles);
+
 router.post("/fields", cargosnapController.fieldsFiles);
 router.post("/reports", cargosnapController.reportsFiles);
 router.get("/share", cargosnapController.shareFiles);
