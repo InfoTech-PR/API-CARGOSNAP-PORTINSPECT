1219a17b306bcd2667d636bca1589988b727f19b
mauboru <josue21henrique@gmail.com>
2025-03-05 11:18:40 -0300

    corrigido config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apps:[
+    apps: [
         {
             name: "API-CARGOSNAP-PORTINSPECT",
             script: "./src/server.ts",
             instances: 1,
-            interpreter: "fork",
+            interpreter: "ts-node", 
             env: {
                 NODE_ENV: "development",
             },
@@ -12,7 +12,7 @@ module.exports = {
             ignore_watch: ["node_modules", "logs"],
             error_file: "logs/error.log",
             out_file: "logs/out.log",
-            log_data_format: "YYYY-MM-DD HH:mm:ss"
+            log_date_format: "YYYY-MM-DD HH:mm:ss"
         }
     ]
 }
